30b5fd46c569cef789335bdba509025be99c6e62
tzl <3300541386@qq.com>
2021-03-15 16:47:06 +0800

    tzl315

--- src/router/index.js
+++ src/router/index.js
@@ -4,90 +4,90 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
-    path: '/',
-    redirect: '/login'
-  }, {
-    path: '/home',
-    name: 'Home',
-    component: () =>
-      import( /* webpackChunkName:home */ './../views/Home.vue'),
-    children: [{
         path: '/',
-        redirect: 'welcome'
-      },
-      {
-        path: "users",
-        name: "Users",
+        redirect: '/login'
+    }, {
+        path: '/home',
+        name: 'Home',
         component: () =>
-          import( /* webpackChunkName:users */ '../components/home_children/users.vue'),
-      },
-      {
-        path: "roles",
-        name: "Roles",
+            import('./../views/Home.vue'),
+        children: [{
+                path: '/',
+                redirect: 'welcome'
+            },
+            {
+                path: "users",
+                name: "Users",
+                component: () =>
+                    import('../components/home_children/users.vue'),
+            },
+            {
+                path: "roles",
+                name: "Roles",
+                component: () =>
+                    import('../components/home_children/roles.vue'),
+            },
+            {
+                path: "rights",
+                name: "Rights",
+                component: () =>
+                    import('../components/home_children/rights.vue'),
+            },
+            {
+                path: "goods",
+                name: "Goods",
+                component: () =>
+                    import('../components/home_children/goods.vue'),
+            },
+            {
+                path: "params",
+                name: "Params",
+                component: () =>
+                    import('../components/home_children/params.vue'),
+            },
+            {
+                path: "categories",
+                name: "Categories",
+                component: () =>
+                    import('../components/home_children/categories.vue'),
+            },
+            {
+                path: "orders",
+                name: "Orders",
+                component: () =>
+                    import('../components/home_children/orders.vue'),
+            },
+            {
+                path: "reports",
+                name: "Reports",
+                component: () =>
+                    import('../components/home_children/reports.vue'),
+            },
+            {
+                path: "welcome",
+                name: "Welcome",
+                component: () =>
+                    import('../components/home_children/welcome.vue'),
+            },
+            {
+                path: "addgoods",
+                name: "addgoods",
+                component: () =>
+                    import('../components/home_goods/add_goods.vue'),
+            },
+        ]
+    },
+    //login
+    {
+        path: '/login',
+        name: 'Login',
         component: () =>
-          import( /* webpackChunkName:roles */ '../components/home_children/roles.vue'),
-      },
-      {
-        path: "rights",
-        name: "Rights",
-        component: () =>
-          import( /* webpackChunkName:rights */ '../components/home_children/rights.vue'),
-      },
-      {
-        path: "goods",
-        name: "Goods",
-        component: () =>
-          import( /* webpackChunkName:goods */ '../components/home_children/goods.vue'),
-      },
-      {
-        path: "params",
-        name: "Params",
-        component: () =>
-          import( /* webpackChunkName:params */ '../components/home_children/params.vue'),
-      },
-      {
-        path: "categories",
-        name: "Categories",
-        component: () =>
-          import( /* webpackChunkName:categories */ '../components/home_children/categories.vue'),
-      },
-      {
-        path: "orders",
-        name: "Orders",
-        component: () =>
-          import( /* webpackChunkName:orders */ '../components/home_children/orders.vue'),
-      },
-      {
-        path: "reports",
-        name: "Reports",
-        component: () =>
-          import( /* webpackChunkName:reports */ '../components/home_children/reports.vue'),
-      },
-      {
-        path: "welcome",
-        name: "Welcome",
-        component: () =>
-          import( /* webpackChunkName:welcome */ '../components/home_children/welcome.vue'),
-      },
-      {
-        path: "addgoods",
-        name: "addgoods",
-        component: () =>
-          import( /* webpackChunkName:addgoods */ '../components/home_goods/add_goods.vue'),
-      },
-    ]
-  },
-  //login
-  {
-    path: '/login',
-    name: 'Login',
-    component: () =>
-      import( /* webpackChunkName:login */ './../views/Login.vue')
-  }
+            import('./../views/Login.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
